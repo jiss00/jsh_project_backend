ff318bb70d6eb172b712b391023718e3c6438488
jiss00 <ksksksk729@gmail.com>
2023-11-18 15:16:06 +0900

    testx

--- src/test/java/com/example/jsh_project/JshProjectApplicationTests.java
+++ src/test/java/com/example/jsh_project/JshProjectApplicationTests.java
@@ -3,7 +3,6 @@ package com.example.jsh_project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JshProjectApplicationTests {
 
 	@Test
