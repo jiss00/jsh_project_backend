b7d5d67e574919a7eb53b4a1900f2d8e85187e7e
jiss00 <ksksksk729@gmail.com>
2023-11-19 23:19:56 +0900

    db주소 수정

--- src/test/java/com/example/jsh_project/JshProjectApplicationTests.java
+++ src/test/java/com/example/jsh_project/JshProjectApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.jsh_project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class JshProjectApplicationTests {
 
 	@Test
